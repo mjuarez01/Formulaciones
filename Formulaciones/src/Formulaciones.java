50383d2cc830eddb295ee22b9b7e06954ded3ab8
Miguel Juarez <miguel.juarez9@gmail.com>
2013-05-28 18:51:02 -0500

    Iniciando codificacion

---
+++ Formulaciones/src/Formulaciones.java
@@ -0,0 +1,14 @@
+/**
+ * Clase principal, llama a la ventana de login
+ * @author miguel
+ * @version 0.1
+ */
+public class Formulaciones {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
